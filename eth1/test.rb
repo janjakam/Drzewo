3f7ed071c4e82b9f9fbcb4a02f29626f0485c7a8
janjakam <62722500+janjakam@users.noreply.github.com>
2020-12-22 23:21:20 +0100

    Create test.rb

---
+++ eth1/test.rb
@@ -0,0 +1,25 @@
+require 'json'
+require 'rest-client'
+require 'digest/sha3'
+require 'pstore'
+require_relative "utils"
+require_relative "EtherscanAPI"
+
+
+api_key = File.file?('api.key') ? File.open('api.key').read : ''
+
+sl = sl = SimpleLog.new({:verbose => true})
+sl.p "\n\n----------{ running #{$0} - #{Time.now.utc} }----------\n\n"
+
+e = EtherscanAPI.new(api_key, sl, {:print_query => true})
+
+############################################################################### 
+#
+# Block number and ETH price in USD
+#
+
+res = e.get_eth_blockNumber()
+sl.p 'Block number: ' + res.inspect
+
+res = e.get_ethusd()
+sl.p 'ETH/USD: ' + res.inspect
